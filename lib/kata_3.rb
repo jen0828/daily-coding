c47e13d0d2def4e61f42ca0f2a55363e61127dea
Jenny Moon <jenny0moon@gmail.com>
2021-09-11 18:43:57 +0100

    Test: for single number

---
+++ lib/kata_3.rb
@@ -0,0 +1,3 @@
+def to_number_array(string_array)
+  string_array.map{|x| x.to_i}
+end
---
+++ spec/kata_3_spec.rb
@@ -0,0 +1,15 @@
+# Some really funny web dev gave you a sequence of numbers from his API response as an sequence of strings!
+# You need to cast the whole array to the correct type.
+# Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
+# ie:["1", "2", "3"] to [1, 2, 3]
+# Note that you can receive floats as well.
+# def to_number_array(string_array)
+# end
+
+require 'kata_3'
+
+describe '#to_number_array' do
+  it 'should return array converted to numbers'do
+  expect(to_number_array(["1"])).to eq([1])
+  end
+end
