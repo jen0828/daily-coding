4ce524101a0a93b15d0a9ea01839db7f48fb21da
Jenny Moon <jenny0moon@gmail.com>
2021-09-12 20:17:33 +0100

    Test: should return an array when input empty string

---
+++ lib/kata_4.rb
@@ -0,0 +1,3 @@
+def longest_repetition(chars)
+  ["", 0]
+end
---
+++ spec/kata_4_spec.rb
@@ -0,0 +1,19 @@
+# For a given string s find the character c (or C) 
+#with longest consecutive repetition and return:
+# [c, l]
+# where l (or L) is the length of the repetition. 
+#If there are two or more characters with the same l return the first in order of appearance.
+# For empty string return:
+# ["", 0]
+
+# def longest_repetition chars
+#   "Implement me!"
+# end
+
+require "kata_4"
+
+describe "#longest_repetition" do
+  it "should return an array when input empty string" do
+  expect(longest_repetition("")).to eq(["", 0])
+  end
+end
