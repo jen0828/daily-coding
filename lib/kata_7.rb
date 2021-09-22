9f3592f16049c1c1a1e569144488dfbaaa28f638
Jenny Moon <jenny0moon@gmail.com>
2021-09-22 19:13:12 +0100

    Fix tests

--- lib/kata_7.rb
+++ lib/kata_7.rb
@@ -1,7 +1,11 @@
 def ten_min_walk?(arr)
-  if arr.count == 10 && arr.count('w') == arr.count('e')
-    true
-  else
+  if arr.count == 10 
+     if arr.count('w') == arr.count('e') && arr.count('s') == arr.count('n')
+     true
+     else
+     false 
+     end
+  elsif arr.count < 10
     false
   end
 end
--- spec/kata_7_spec.rb
+++ spec/kata_7_spec.rb
@@ -44,7 +44,11 @@ describe 'ten_min_walk?' do
     expect(ten_min_walk?(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).to be true
   end
 
-  it 'should return true if the walk take ten minutes exactly' do
+  it 'should return false if the walk does not take ten minutes exactly' do
     expect(ten_min_walk?(['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w'])).to be false
+  end
+
+  it 'should return false if the walk dose not take ten minutes exactly' do
+    expect(ten_min_walk?(['w', 's', 'e', 's', 's', 'e', 's', 'w', 'n', 'n'])).to be false
   end
 end
