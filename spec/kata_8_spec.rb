1665903aab575ce1bfbbd2d3669419a84705fac7
Jenny Moon <jenny0moon@gmail.com>
2021-09-24 10:06:11 +0100

    Fix typo

--- spec/kata_8_spec.rb
+++ spec/kata_8_spec.rb
@@ -8,7 +8,7 @@ describe Scrabble do
       expect(subject.score('A')).to eq(1)
     end
 
-    it 'should return 1 when input letter A ' do
+    it 'should return 1 when input letter a ' do
       expect(subject.score('a')).to eq(1)
     end
   end
