77a32dea34d09495ae13deecd489f0b0bacb0359
Jenny Moon <jenny0moon@gmail.com>
2021-09-28 19:43:46 +0100

    Test: should return -1 when argument is not a capital letter

--- lib/kata_9.rb
+++ lib/kata_9.rb
@@ -1,4 +1,5 @@
 class Shop
   def checkout(str_capital)
+    return -1 if str_capital != str_capital.upcase
   end
 end
--- spec/kata_9_spec.rb
+++ spec/kata_9_spec.rb
@@ -2,4 +2,10 @@ require 'kata_9'
 
 describe Shop do
   it { is_expected.to respond_to(:checkout).with(1).arguments }
+
+  describe '#checkout' do
+    it 'should return -1 when argument is not a capital letter' do
+    expect(subject.checkout('a')).to eq(-1)
+    end
+  end
 end
