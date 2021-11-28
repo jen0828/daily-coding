99495b27c44266bcf3bf713abbb22d1b1278729e
Jenny Moon <jenny0moon@gmail.com>
2021-11-28 15:19:20 +0000

    Test: use doubles for bank accounts

--- mocking/lib/bank.rb
+++ mocking/lib/bank.rb
@@ -1,9 +1,13 @@
 class Bank
+  attr_reader :accounts
+
   def initialize(accounts)
     @accounts = accounts
   end
 
   def count_overdrawn_account
-    0
+    @accounts.select {|account|
+    account.balance < 0
+    }.count
   end
 end
--- mocking/spec/bank_spec.rb
+++ mocking/spec/bank_spec.rb
@@ -1,6 +1,7 @@
 require 'bank'
 
 # I would like to keep track of my bank to keep track of the bank accounts
+# I would like to know how many bank accounts are overdrawn
 
 describe Bank do
   describe '#count_overdrawn_account' do
@@ -14,6 +15,21 @@ describe Bank do
         expect(bank.count_overdrawn_account).to eq(0)
       end
     end
-  end
-  
+
+    context '1 overdrawn account and 1 account is not overdrawn ' do
+      it 'return 1' do
+
+        account = double('bank_account')
+        allow(account).to receive(:balance).and_return(0)
+
+        overdrawn_account = double('overdrawn_account')
+        allow(overdrawn_account).to receive(:balance).and_return(-1)
+        
+        accounts = [account, overdrawn_account]
+        bank = Bank.new(accounts)
+
+        expect(bank.count_overdrawn_account).to eq(1)
+      end
+    end
+  end  
 end
