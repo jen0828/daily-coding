996fdc4f496c5bcda6d1b92de60bf6341ea150c8
Jenny Moon <jenny0moon@gmail.com>
2021-12-04 12:30:53 +0000

    Refactor

--- lib/kata_9.rb
+++ lib/kata_9.rb
@@ -1,19 +1,23 @@
 class Shop
+
+  def initialize
+    @price = { 'A' => 50, 'B' => 30, 'C' => 20, 'D' => 15 }
+  end
+
   def checkout(str_capital)
     raise 'your basket is empty!' if str_capital == ''
-
-    price = { 'A' => 50, 'B' => 30, 'C' => 20, 'D' => 15 }
 
     if str_capital.is_a? String
 
       if str_capital != str_capital.upcase
         -1
       else 
-        str_capital.chars.map { |letter| price[letter] }.reduce(:+)
+        str_capital.chars.map { |letter| @price[letter] }.reduce(:+)
       end
-
     else
       -1
     end
   end
 end
+
+
